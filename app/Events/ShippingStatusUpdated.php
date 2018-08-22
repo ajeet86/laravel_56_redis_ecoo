88a0be32471a14b5ae0bc1a6a8a8cbf14d6083dd
Ajeet Kumar <ajeetkumar@virtual.local>
2018-08-22 19:54:09 +0530

    push all local files to repostiory

---
+++ app/Events/ShippingStatusUpdated.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ShippingStatusUpdated  implements  ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+	 
+	 public $update;
+    public function __construct()
+    {
+        //
+		//$this->update->order_id='100';
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+       // return new PrivateChannel('channel-name');
+		 return new Channel('order1');
+		
+    }
+	
+	public function broadcastWith()
+	{
+		return ['data111' => 'key111'];
+	}
+}
